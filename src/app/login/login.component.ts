4fdd98cfb59665bd9e00d75eedb932a59beeda1f
taotara <taotara@yahoo.com>
2023-07-05 08:59:13 +0100

    done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  loginform!: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private http: HttpClient
+  ) {}
 
+  ngOnInit(): void {
+    this.loginform = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  login() {
+    this.http.get<any>('http://localhost:3000/signup').subscribe(
+      (res) => {
+        // match email and password
+        const user = res.find((a: any) => {
+          return (
+            a.email === this.loginform.value.email &&
+            a.password === this.loginform.value.password
+          );
+        });
+
+        // condition for login
+        if (user) {
+          alert('Successfully logged in');
+          this.loginform.reset();
+          this.router.navigate(['student']);
+        } else {
+          alert('User not found with these credentials');
+        }
+      },
+      (err) => {
+        alert('Something went wrong');
+      }
+    );
+  }
 }
